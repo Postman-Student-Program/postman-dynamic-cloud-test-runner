c677f2e9c535778872ddc3f01d62041e1d2134d6
clairefro <claire.froelich@gmail.com>
2021-10-11 11:11:50 -0400

    Remove unnecessary env var

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import 'dotenv/config'
 import { validator } from './middleware/validator'
 import { handleErrors } from './middleware/handleErrors'
 import { asyncHandler } from './middleware/asyncHandler'
@@ -7,12 +6,6 @@ import { asyncHandler } from './middleware/asyncHandler'
 import { routes } from './routes'
 
 const PORT = process.env.PORT || 3000
-
-const TEST_COLLECTION_URL = process.env.TEST_COLLECTION_URL
-if (!TEST_COLLECTION_URL) {
-  console.error('Missing env var: TEST_COLLECTION_URL. Aborting.')
-  process.exit(1)
-}
 
 const app = express()
 
